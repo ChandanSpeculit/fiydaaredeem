fd5452757858e0df206338ab1d2c34464b061c31
ChandanSpecuilt <chandan007.behera@speculit.com>
2024-02-27 15:51:04 +0530

    50rupees gold

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import UserPage from './UserPage';
-import './App.css'; // Importing CSS for styling
 import logo from './assets/2133.png'; // Import your logo image
 
 const App = () => {
   return (
     <Router>
-      <div className="app-container">
-      <img src={logo} alt="Logo" className="logo"/>
-
-        <h1>Welcome </h1>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', flexDirection: 'column' }}>
+        <div style={{ textAlign: 'center' }}>
+          <img src={logo} alt="Logo" style={{ width: '100px', height: 'auto' }} />
+          <h1>Welcome</h1>
+        </div>
         <Routes>
           <Route path="/user/:userId" element={<UserPage />} />
         </Routes>
